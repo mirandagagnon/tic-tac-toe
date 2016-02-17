06ba3d4227777b123229d42482583a507cd4c07e
Miranda Gagnon <miranda.kay.gagnon@gmail.com>
2016-02-16 21:51:52 -0500

    first commit

---
+++ ticTacToe.js
@@ -0,0 +1,70 @@
+$(document).on('ready', function(){
+  var turn = 0;
+  var xSelections = [];
+  var oSelections = [];
+
+  $('td').on('click', function(){
+    var self = $(this);
+
+    if(turn % 2 === 0){
+      self.html('X').addClass('x');
+      xSelections.push(parseInt(self.attr('id').split('')[2]));
+      console.log(xSelections);
+    }else{
+      self.html('O').addClass('o');
+      oSelections.push(parseInt(self.attr('id').split('')[2]));
+      console.log(oSelections);
+    }
+
+    self.off('click');
+
+    checkForWinner();
+
+    turn++;
+
+  });
+
+  function xWins(){
+    var xTotal = 0;
+    $.each(xSelections, function(){
+    xTotal += this;
+    });
+    if(xTotal === 15){
+      alert("X wins!");
+      if(confirm("Would you like to play again?")){
+        location.reload();
+      }
+    }
+  }
+
+  function oWins(){
+    var oTotal = 0;
+    $.each(oSelections, function(){
+    oTotal += this;
+    });
+    if(oTotal === 15){
+      alert("O wins!");
+      if(confirm("Would you like to play again?")){
+        location.reload();
+      }
+    }
+  }
+
+  function checkForWinner(){
+    if(turn === 4){
+      xWins();
+    }else if(turn === 5){
+      oWins();
+    }else if(turn === 6){
+      xWins();
+    }else if(turn === 7){
+      oWins();
+    }else if(turn === 8){
+      alert("Draw!");
+      if(confirm("Would you like to play again?")){
+        location.reload();
+      }
+    }
+  }
+
+});
